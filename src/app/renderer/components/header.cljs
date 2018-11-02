0ba1831b3e0bd200404f82c5e9da00de63ebcd25
mrrbrilliant <mrrbrilliantwork@gmail.com>
2018-11-02 16:18:57 +0700

    UI update

--- src/app/renderer/components/header.cljs
+++ src/app/renderer/components/header.cljs
@@ -12,25 +12,25 @@
     (swap! activeTab assoc :tab id))
 
 (defn header []
-(println @tabs)
-    [:div {:className "header"}
-        [:div {:className "titleBar"} "KOSMOS DOCUMENT"]
-        [:div {:className "menuBar"}
-            [:ul
-            [:li "File"]
-            (doall (map (fn [tab]
-                [:li
-                    {
-                        :key (:id tab)
-                        :className (if (=(get-in @activeTab [:tab]) (:id tab)) "active" "")
-                        :onClick (fn [] (switchTab (:id tab)))
-                    }
-                (:name tab)]) @tabs))]]
+ (println @tabs)
+ [:div {:className "header"}
+     [:div {:className "titleBar"} "KOSMOS DOCUMENT"]
+     [:div {:className "menuBar"}
+         [:ul
+          [:li "File"]
+          (doall (map (fn [tab]
+                       [:li
+                           {
+                               :key (:id tab)
+                               :className (if (=(get-in @activeTab [:tab]) (:id tab)) "active" "")
+                               :onClick (fn [] (switchTab (:id tab)))}
 
-        [:div {:className "ribbonContainer"}
-            (if (= (get-in @activeTab [:tab]) "1") [homeTab])
-            (if (= (get-in @activeTab [:tab]) "2") [insertTab])
-            (if (= (get-in @activeTab [:tab]) "3") [layoutTab])
-            (if (= (get-in @activeTab [:tab]) "4") [referencesTab])
-            (if (= (get-in @activeTab [:tab]) "5") [reviewTab])
-            (if (= (get-in @activeTab [:tab]) "6") [viewTab])]])
+                        (:name tab)]) @tabs))]]
+
+     [:div {:className "ribbonContainer"}
+         (if (= (get-in @activeTab [:tab]) "1") [homeTab])
+         (if (= (get-in @activeTab [:tab]) "2") [insertTab])
+         (if (= (get-in @activeTab [:tab]) "3") [layoutTab])
+         (if (= (get-in @activeTab [:tab]) "4") [referencesTab])
+         (if (= (get-in @activeTab [:tab]) "5") [reviewTab])
+         (if (= (get-in @activeTab [:tab]) "6") [viewTab])]])
--- src/app/renderer/components/insertTab.cljs
+++ src/app/renderer/components/insertTab.cljs
@@ -3,5 +3,14 @@
 (defn insertTab
   []
   [:div {:className "ribbonMainGroup"}
-      [:ul
-        [:li {:data "Undo" :className "ribbonIcon" :style {:backgroundImage "url('img/undo.png')"}}]]])
+   [:ul
+    [:li {:data "Break Page" :className "ribbonIconwithText" :style {:backgroundImage "url('img/pageBreak.png')"}} "Page Break"]
+    [:li {:data "Create Table" :className "ribbonIconwithText" :style {:backgroundImage "url('img/table.png')"}} "Table"]
+    [:li {:data "Insert Picture" :className "ribbonIconwithText" :style {:backgroundImage "url('img/picture.png')"}} "Picture"]
+    [:li {:data "Insert Link" :className "ribbonIconwithText" :style {:backgroundImage "url('img/link.png')"}} "Link"]
+    [:li {:data "Give Comments" :className "ribbonIconwithText" :style {:backgroundImage "url('img/comment.png')"}} "Comment"]
+    [:li {:data "Header & Footer" :className "ribbonIconwithText" :style {:backgroundImage "url('img/headerFooter.png')"}} "Header & Footer"]
+    [:li {:data "Page Number" :className "ribbonIconwithText" :style {:backgroundImage "url('img/pageNumber.png')"}} "Page Numbers"]
+    [:li {:data "Break Page" :className "ribbonIconwithText" :style {:backgroundImage "url('img/symbols.png')"}} "Symbols"]
+    [:li {:data "Break Page" :className "ribbonIconwithText" :style {:backgroundImage "url('img/emoji.png')"}} "Emoji"]
+    [:li {:data "Break Page" :className "ribbonIconwithText" :style {:backgroundImage "url('img/sticker.png')"}} "Stickers"]]])
